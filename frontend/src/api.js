57f810059239a3e1c45de43aea09299a9defea3f
Nejra <nejra_lacevic@hotmail.com>
2025-06-15 22:07:19 +0200

    backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE = "/api";
+const API_BASE = "https://usersnack-production.up.railway.app/api";
+// use "api" for local
 
 export const fetchPizzas = () => axios.get(`${API_BASE}/pizza/all`);
 export const fetchExtras = () => axios.get(`${API_BASE}/extra/all`);
