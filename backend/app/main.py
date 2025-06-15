79e78839816f81f7abcc25c66e041f72eadc6ca1
Nejra <nejra_lacevic@hotmail.com>
2025-06-15 22:09:29 +0200

    cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
 from app.router import pizza, order
 
 app = FastAPI(title="Usersnack Pizza API")
+
+origins = [
+    "https://jolly-horse-7f6ff4.netlify.app",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(pizza.router, prefix="/api", tags=["Pizzas"])
 app.include_router(order.router, prefix="/api", tags=["Order"])
